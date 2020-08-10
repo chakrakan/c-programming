87cf9aed8c86848f646314facaf9dffbfd5a793c
Coursera Learner <nobody@nowhere.nul>
2020-08-10 01:21:21 +0000

    attemp 2

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 double get_balance(double get_balance, double rate);
 
-typedef struct _retire_info {
+// declare a struct _retire_info
+struct _retire_info {
   int months;
   double contribution;
   double rate_of_return;
-} retire_info;
+}
+// use typedef to make retire_info
+typedef struct _retire_info retire_info;
 
+// retirement function
 void retirement(int startAge, double initial, retire_info working,
 		retire_info retired) {
   // first, compute retirement account balance each month (working)
@@ -25,10 +29,12 @@ void retirement(int startAge, double initial, retire_info working,
     }  
 }
 
+// abstract out get balance function
 double get_balance(double balance, double rate_of_return) {
   return balance * (1 + rate_of_return / 12);
 }
 
+// main function
 int main(void) {
   retire_info working = { 489, 1000, 0.045 };
   retire_info retired = { 384, -4000, 0.01 };
